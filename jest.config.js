268c9f22b19b0cbcd090087457fdca5c9665e1a0
Jimmy Liao <jimmy.liao@gogoro.com>
2024-05-16 15:13:15 +0800

    Remove unnecessary stub

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,5 @@ module.exports = {
     '^@/(.*)': '<rootDir>/src/$1',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
-    '^.+\\.(css|less)$': '<rootDir>/__tests__/helper/CSSStub.ts',
   },
 };
