f503b03b8d0dd8effbf7b02d57602030fc2c1dd5
Jimmy Liao <jimmy.liao@gogoro.com>
2024-06-17 17:02:05 +0800

    Fix 18 typo

--- src/utils/Age.ts
+++ src/utils/Age.ts
@@ -1,12 +1,12 @@
-const EIGHTTEEN_YEARS = 18 * 365 * 24 * 60 * 60 * 1000;
+const EIGHTEEN_YEARS = 18 * 365 * 24 * 60 * 60 * 1000;
 
 // export const isOver18YearsOld = (birthDate: Date) => {
 //   const current = new Date();
-//   return current.getTime() - birthDate.getTime() >= EIGHTTEEN_YEARS;
+//   return current.getTime() - birthDate.getTime() >= EIGHTEEN_YEARS;
 // };
 
 export const isOver18YearsOld = (current: Date, birthDate: Date) => {
-  return current.getTime() - birthDate.getTime() >= EIGHTTEEN_YEARS;
+  return current.getTime() - birthDate.getTime() >= EIGHTEEN_YEARS;
 };
 
 export default undefined;
